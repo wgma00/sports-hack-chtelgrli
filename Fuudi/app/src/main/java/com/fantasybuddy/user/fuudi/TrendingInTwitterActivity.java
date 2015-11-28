bddd527e1fce2af95bb64acf4d91a6da95555195
Avarrocka <catzwillruletheearth@gmail.com>
2015-11-28 16:55:41 -0500

    Twitter feed final fix

--- Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/TrendingInTwitterActivity.java
+++ Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/TrendingInTwitterActivity.java
@@ -1,17 +1,61 @@
 package com.fantasybuddy.user.fuudi;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TrendingInTwitterActivity extends AppCompatActivity {
-
-    @Override
+    ArrayList<Tweet> tweets = new ArrayList<Tweet>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trending_in_twitter);
+    }
+    public void loadPlayerTrends(View view){
+        TextView playerInfo = (TextView) findViewById(R.id.playerTweets);
+        EditText playerNameBox = (EditText) findViewById(R.id.playerNameBox);
+        String playerName = playerNameBox.getText().toString();
+        ArrayList<String> results = new ArrayList<String>();
+        enableStrictMode();
+        results = new TwitterUpdates().returnTweets(playerName);
+        StrictMode.enableDefaults();
+        for(int i = 0; i < results.size(); i++){
+            String result = results.get(i);
+            String handle = result.substring(0, result.indexOf(":")+1);
+            String message =  result.substring(result.indexOf(":")+1, result.length()-1);
+            tweets.add(new Tweet(handle, message));
+        }
+        for(int i = 0; i < tweets.size(); i++){
+            Tweet tweetRN = tweets.get(i);
+            playerInfo.setText(playerInfo.getText() + "\n" + tweetRN.handle + "\r" + tweetRN.message + "\n");
+        }
+    }
+
+    public class Tweet{
+        String handle;
+        String message;
+        public Tweet(String handle, String message){
+            this.handle = handle;
+            this.message = message;
+        }
+    }
+
+    public void enableStrictMode()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 }
--- Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/TwitterUpdates.java
+++ Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/TwitterUpdates.java
@@ -1,48 +1,54 @@
-//package com.fantasybuddy.user.fuudi;
-//
-//import java.util.ArrayList;
-//import java.util.*;
-//import twitter4j.*;
-//import twitter4j.auth.*;
-//import twitter4j.conf.ConfigurationBuilder;
-//
-//public class TwitterUpdates {
-//	public static String htmlFilePath;
-//	public static Twitter twitter;
-//	public static RequestToken requestToken = null;
-//	public static AccessToken accessToken = null;
-//	public static String pin;
-//	public static ConfigurationBuilder cb = new ConfigurationBuilder();
-//
-//	public TwitterUpdates(){
-//		initTwitter();
-//	}
-//
-//	public ArrayList<String> returnTweets(String nameToSearch){
-//		String name = nameToSearch;
-//		ArrayList<String> tweets = new ArrayList<>();
-//		try {
-//			Query query = new Query(name);
-//		    QueryResult result = twitter.search(query);
-//		    for (Status status : result.getTweets()) {
-//		    	tweets.add("@" + status.getUser().getScreenName() + ": \n" + status.getText());
-//		    }
-//		}
-//		catch (TwitterException te) {
-//			te.printStackTrace();
-//		}
-//		return tweets;
-//	}
-//
-//	public static void initTwitter(){
-//		cb.setDebugEnabled(true)
-//		.setOAuthConsumerKey("nQZ4c9w1beTSPR4fT7HaJ1hGN")
-//		.setOAuthConsumerSecret(
-//				"dMXkSXV0FZDGTyQdBOypHxii8Xty8r0QqqGUjwBfozpoSU7m6q")
-//		.setOAuthAccessToken(
-//				"3192494632-s3SRp7ZEKLjbgJE3c5eYap8TUyxEOy4mHRFMdJO")
-//		.setOAuthAccessTokenSecret(
-//				"6mODPqDKXvOtvSrNNYbaQFGN3MSKR3cmCVAElAI9ZUSZp");
-//		twitter = new TwitterFactory(cb.build()).getInstance();
-//	}
-//}
+package com.fantasybuddy.user.fuudi;
+
+import java.util.ArrayList;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterUpdates {
+	public static String htmlFilePath;
+	public static Twitter twitter;
+	public static RequestToken requestToken = null;
+	public static AccessToken accessToken = null;
+	public static String pin;
+	public static ConfigurationBuilder cb = new ConfigurationBuilder();
+	
+	public TwitterUpdates(){
+		initTwitter();
+	}
+	
+	public ArrayList<String> returnTweets(String nameToSearch){
+		String name = nameToSearch;
+		ArrayList<String> tweets = new ArrayList<>();
+		try {
+			Query query = new Query(name);
+		    QueryResult result = twitter.search(query);
+		    for (Status status : result.getTweets()) {
+		    	tweets.add("@" + status.getUser().getScreenName() + ": \n" + status.getText());
+		    }
+		}
+		catch (TwitterException te) {
+			te.printStackTrace();
+		}
+		return tweets;
+	}
+	
+	public static void initTwitter(){
+		cb.setDebugEnabled(true)
+		.setOAuthConsumerKey("nQZ4c9w1beTSPR4fT7HaJ1hGN")
+		.setOAuthConsumerSecret(
+				"dMXkSXV0FZDGTyQdBOypHxii8Xty8r0QqqGUjwBfozpoSU7m6q")
+		.setOAuthAccessToken(
+				"3192494632-s3SRp7ZEKLjbgJE3c5eYap8TUyxEOy4mHRFMdJO")
+		.setOAuthAccessTokenSecret(
+				"6mODPqDKXvOtvSrNNYbaQFGN3MSKR3cmCVAElAI9ZUSZp");
+		twitter = new TwitterFactory(cb.build()).getInstance();
+	}
+}
