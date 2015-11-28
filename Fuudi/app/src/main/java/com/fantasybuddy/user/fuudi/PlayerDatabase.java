cd533cfc96c51b45d882cc87466417a7f6c25fd2
Justin <Justin.li.jl@gmail.com>
2015-11-28 09:51:56 -0500

    PlayerDatabase implemented
    Uses PlayerTextFileHandler to get a list of players
    Then assigns them to a HashTable by (FIRSTNAME LASTNAME)
    and another HashTable by id

--- Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/PlayerDatabase.java
+++ Fuudi/app/src/main/java/com/fantasybuddy/user/fuudi/PlayerDatabase.java
@@ -4,6 +4,8 @@ package com.fantasybuddy.user.fuudi;
  * Created by William G on 11/28/2015.
  */
 import android.content.Context;
+
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class PlayerDatabase {
@@ -11,12 +13,35 @@ public class PlayerDatabase {
     Hashtable<Integer, Player>playerIdDatabase;
     Hashtable<String, Player>playerNameDatabase;
 
+    ArrayList<Player> playerList;
+
     Context context;
 
     public PlayerDatabase(Context context){
         this.context = context;
-    
+
+        updateDatabases();
     }
 
+    private void updateDatabases(){
+        PlayerTextFileHandler textHandler = new PlayerTextFileHandler();
+        playerList = textHandler.getPlayers();
 
+        for(int i = 0; i < playerList.size(); i++){
+            Player currentPlayer = playerList.get(i);
+            String playerName = currentPlayer.getFirstName() + " " + currentPlayer.getLastName();
+            int playerId = currentPlayer.getId();
+
+            playerIdDatabase.put(playerId, currentPlayer);
+            playerNameDatabase.put(playerName, currentPlayer);
+        }
+    }
+
+    public Hashtable<Integer, Player> getPlayerIdDatabase(){
+        return playerIdDatabase;
+    }
+
+    public Hashtable<String, Player> getPlayerNameDatabase(){
+        return playerNameDatabase;
+    }
 }
